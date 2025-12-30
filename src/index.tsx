a7eb897fd232b980441fd4ba76802282fd96dcce
Jan Nemecek <honzanemecek@slimac.local>
2025-12-30 09:32:33 +0100

    feat: apply theme prop

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "@mui/material/styles";
+import "./theme.d";
 import App from "./App";
+import { customTheme } from "./theme";
 
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={customTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
--- src/theme.d.ts
+++ src/theme.d.ts
@@ -1,28 +1,35 @@
 export {};
 
 declare module "@mui/material/InputBase" {
+  interface InputBaseOwnProps {
+    /** if true, the input will have a warning color, error has higher priority */
+    warning?: boolean;
+  }
   interface InputBaseProps {
-    /* if true, the input will have a warning color, error has higher priority */
+    warning?: boolean;
+  }
+}
+
+declare module "@mui/material/OutlinedInput" {
+  interface OutlinedInputOwnProps {
+    /** if true, the input will have a warning color, error has higher priority */
+    warning?: boolean;
+  }
+  interface OutlinedInputProps {
     warning?: boolean;
   }
 }
 
 declare module "@mui/material/TextField" {
   interface BaseTextFieldProps {
-    /* if true, the input will have a warning color, error has higher priority */
+    /** if true, the input will have a warning color, error has higher priority */
     warning?: boolean;
   }
 }
 
 declare module "@mui/material/FormHelperText" {
   interface FormHelperTextOwnProps {
-    /* if true, the input will have a warning color, error has higher priority */
+    /** if true, the input will have a warning color, error has higher priority */
     warning?: boolean;
-  }
-}
-
-declare module "@mui/material/OutlinedInput" {
-  interface OutlinedInputProps {
-    warning: boolean;
   }
 }
